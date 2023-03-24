722bfd6e6a7c2ca33a2a8084beff29fb2b312594
shdb <shadowblade@gmx.ch>
2023-03-24 01:21:33 +0100

    lazily load telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -22,6 +22,12 @@ end
 
 return {
     'nvim-telescope/telescope.nvim',
+    keys = {
+        { '<leader>ff', '<cmd>lua require("telescope.builtin").find_files()<cr>', desc = 'telescope find files' },
+        { '<leader>fg', '<cmd>lua require("telescope.builtin").live_grep()<cr>',  desc = 'telescope live grep' },
+        { '<leader>fb', '<cmd>lua require("telescope.builtin").buffers()<cr>',    desc = 'telescope buffers' },
+        { '<leader>fh', '<cmd>lua require("telescope.builtin").help_tags()<cr>',  desc = 'telescope help tags' },
+    },
     config = function()
         getconfig()
     end
