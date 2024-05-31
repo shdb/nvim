f994c12d90e9252feb3ada6c293a9733cde2e14b
shdb <shadowblade@gmx.ch>
2024-05-31 14:55:00 +0200

    add mini.nvim

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,6 +1,5 @@
 return {
     'nvim-lua/plenary.nvim', -- Useful lua functions used by lots of plugins
     'chrisbra/unicode.vim',
-    'junegunn/vim-easy-align',
     'navarasu/onedark.nvim', -- Theme
 }
