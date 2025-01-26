97d4d441edad6e7210f39070e81b9959b17ca1c7
shdb <shadowblade@gmx.ch>
2025-01-26 23:33:06 +0100

    do not remove trailing whitespaces from markdown files

--- lua/user/misc.lua
+++ lua/user/misc.lua
@@ -1,4 +1,9 @@
 vim.api.nvim_create_autocmd({'BufWritePre'}, {
-    pattern = {'*'},
-    command = ':%s/\\s\\+$//e',  -- remove trailing whitespace on save
+    pattern = '*',
+    callback = function(env)
+        if vim.bo.filetype == 'markdown' then  -- markdown files can have trailing whitespaces
+            return
+        end
+        vim.cmd('%s/\\s\\+$//e')  -- remove trailing whitespaces on save
+    end
 })
