f267466e6d0db9d9cd3e98e06068d108c2b27135
shdb <shadowblade@gmx.ch>
2025-02-07 23:02:05 +0100

    update nvim-web-devicons repository

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -2,7 +2,7 @@ return {
     'lewis6991/gitsigns.nvim',
     event = "BufReadPost",
     dependencies = {
-        'kyazdani42/nvim-web-devicons', -- optional, for file icon
+        'nvim-tree/nvim-web-devicons', -- optional, for file icon
     },
     config = function()
         require('gitsigns').setup()
