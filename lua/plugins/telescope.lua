04ebcf829b38bd41c64c3065e1e6bb6555d9d7ab
shdb <shadowblade@gmx.ch>
2025-03-31 18:39:04 +0200

    add keymap descriptions to telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -4,10 +4,10 @@ return {
         'nvim-lua/plenary.nvim',
     },
     config = function()
-        vim.keymap.set('n', '<leader>ff', require("telescope.builtin").find_files)
-        vim.keymap.set('n', '<leader>fg', require("telescope.builtin").live_grep)
-        vim.keymap.set('n', '<leader>fb', require("telescope.builtin").buffers)
-        vim.keymap.set('n', '<leader>fh', require("telescope.builtin").help_tags)
+        vim.keymap.set('n', '<leader>ff', require("telescope.builtin").find_files, { desc = 'telescope find file' })
+        vim.keymap.set('n', '<leader>fg', require("telescope.builtin").live_grep,  { desc = 'telescope grep' })
+        vim.keymap.set('n', '<leader>fb', require("telescope.builtin").buffers,    { desc = 'telescope buffers' })
+        vim.keymap.set('n', '<leader>fh', require("telescope.builtin").help_tags,  { desc = 'telescope edit Neovim config' })
         vim.keymap.set('n', '<leader>en', function()
             require("telescope.builtin").find_files {
                 cwd = vim.fn.stdpath('config')
