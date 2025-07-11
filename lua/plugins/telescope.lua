5c0ed2a8e170394f925f1c5f1b981d143a363cae
shdb <shadowblade@gmx.ch>
2025-07-11 14:35:09 +0200

    fix telescope bindings descriptions

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -7,11 +7,14 @@ return {
         vim.keymap.set('n', '<leader>ff', require("telescope.builtin").find_files, { desc = 'telescope find file' })
         vim.keymap.set('n', '<leader>fg', require("telescope.builtin").live_grep,  { desc = 'telescope grep' })
         vim.keymap.set('n', '<leader>fb', require("telescope.builtin").buffers,    { desc = 'telescope buffers' })
-        vim.keymap.set('n', '<leader>fh', require("telescope.builtin").help_tags,  { desc = 'telescope edit Neovim config' })
-        vim.keymap.set('n', '<leader>en', function()
-            require("telescope.builtin").find_files {
-                cwd = vim.fn.stdpath('config')
-            }
-        end)
+        vim.keymap.set('n', '<leader>fh', require("telescope.builtin").help_tags,  { desc = 'telescope help' })
+        vim.keymap.set('n', '<leader>en',
+            function()
+                require("telescope.builtin").find_files {
+                    cwd = vim.fn.stdpath('config')
+                }
+            end,
+            { desc = 'telescope neovim config' }
+        )
     end
 }
