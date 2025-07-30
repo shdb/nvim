e29772a0c5d0abfd554a51ee97290d2ff4d2f48f
shdb <shadowblade@gmx.ch>
2025-07-30 20:52:40 +0200

    set additional_vim_regex_highlighting for ncat

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,3 +1,9 @@
+-- make ncat happy
+local vimregex = false
+if vim.v.argv[5] and vim.v.argv[5]:match('NoMatchParen') then
+    vimregex = true
+end
+
 local function getconfig()
     require('nvim-treesitter.configs').setup {
       -- A list of parser names, or "all"
@@ -43,7 +49,7 @@ local function getconfig()
             -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
             -- Using this option may slow down your editor, and you may see some duplicate highlights.
             -- Instead of true it can also be a list of languages
-            additional_vim_regex_highlighting = false,
+            additional_vim_regex_highlighting = vimregex,
         },
     }
 end
