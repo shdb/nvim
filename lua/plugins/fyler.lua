51d02dc75b605a4891304ae9ef46a31963fc8659
shdb <shadowblade@gmx.ch>
2025-08-22 17:22:52 +0200

    <leader>e toggles fyler from any window now

--- lua/plugins/fyler.lua
+++ lua/plugins/fyler.lua
@@ -3,11 +3,6 @@ return {
     dependencies = { 'echasnovski/mini.icons' },
     config = function()
         require('fyler').setup {
-            mappings = {
-                explorer = {
-                        ['<leader>e'] = 'CloseView',
-                },
-            },
             views = {
                 explorer = {
                     win = {
@@ -16,6 +11,17 @@ return {
                 },
             },
         }
-        vim.keymap.set('n', '<leader>e', ':Fyler<cr>', { desc = 'file explorer' })
+
+        vim.keymap.set('n', '<leader>e',
+            function()
+                for _, win in ipairs(vim.api.nvim_list_wins()) do
+                    if string.match(vim.api.nvim_buf_get_name(vim.api.nvim_win_get_buf(win)), '^fyler:') then
+                        return vim.api.nvim_win_close(win, false)
+                    end
+                end
+                require('fyler').open()
+            end,
+            { desc = 'file explorer' }
+        )
     end,
 }
