d3512b70181fefb70aa9cddd5fabfa244e7b9935
shdb <shadowblade@gmx.ch>
2025-10-01 00:10:30 +0200

    use fyler.toggle

--- lua/plugins/fyler.lua
+++ lua/plugins/fyler.lua
@@ -9,12 +9,7 @@ return {
     init = function()
         vim.keymap.set('n', '<leader>e',
             function()
-                for _, win in ipairs(vim.api.nvim_list_wins()) do
-                    if string.match(vim.api.nvim_buf_get_name(vim.api.nvim_win_get_buf(win)), '^fyler:') then
-                        return vim.api.nvim_win_close(win, false)
-                    end
-                end
-                vim.cmd.Fyler()
+                require('fyler').toggle()
             end,
             { desc = 'file explorer' }
         )
